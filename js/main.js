2022ab0ed8bd55ac40dd91cc6c38b8b87338b060
jadins24 <jadinesh04@gmail.com>
2023-01-11 16:30:08 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function myFunction(...value) {
+    console.log(value);   
+}
+myFunction(1,2,3,4);
+
+
+function myFunction1(...value) {
+    console.log(value);   
+}
+myFunction1("red","green","blue");
+
+
+
+function myFunction2(...value) {
+    console.log(value);   
+}
+myFunction2([1,2],[3,4]);
+
+
+
+function myFunction3(...value) {
+    console.log(value);   
+}
+myFunction3([{nvalueme:"dinesh"}]);
+
+
+function myFunction4(...value) {
+    console.log(value);   
+}
+myFunction4([1,2],[{color:"red"}]);
+
+
+function myFunction5(...value) {
+    console.log(value);   
+}
+myFunction5([{color:"red"},{color:"green"},{color:"blue"}]);
+
+
+function myFunction6(...value) {
+    console.log(value);   
+}
+myFunction6([2],["revalued","write"]);
+
+
+function myFunction7(...value) {
+    console.log(value);   
+}
+myFunction7(1,2,2,[1,2,3],[4,5]);
+
+
+function myFunction8(...value) {
+    console.log(value);   
+}
+myFunction8("dinesh",["dd","ds"]);
+
+function myFunction9(...value) {
+    console.log(value);   
+}
+myFunction9("dinesh",["dd","ds"],[{bike:"tvs"}]);
